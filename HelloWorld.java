0a56ab1fbea2fd19f1a94c30b66ebb5fa096b849
Adrijan Vranjkovic <adrijan.vranjkovic@stu.ocsb.ca>
2023-02-07 19:13:18 +0000

    added helloWorld

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// Print Hello World and say Hello to Ms. Raffin
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+      System.out.println("Hello, Ms. Raffin!");
+    }
+}
